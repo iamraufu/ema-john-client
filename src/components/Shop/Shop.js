379aab193bce472977021165fc0c374269877fae
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-26 12:32:06 +0600

    updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,7 +3,7 @@ import fakeData from '../../fakeData';
 import './Shop.css'
 
 const Shop = () => {
-      const f10 = fakeData.slice(0, 5);
+      const f10 = fakeData.slice(0, 20);
       const [products, setProducts] = useState(f10);
       return (
             <div className='container'>
@@ -21,14 +21,14 @@ const Shop = () => {
                                     <p>Items ordered: </p>
                               </div>
                               <div className="cart-summary">
-                              <p>Items: $0
-                              <br/>
+                                    <p>Items: $0
+                              <br />
                               Shipping & Handling: $0
-                              <br/>
+                              <br />
                               Total before tax:	$0
-                              <br/>
+                              <br />
                               Estimated Tax: $0
-                              <br/>
+                              <br />
                               Order Total: $0
                               </p>
                               <button>Review Your Order</button>
@@ -42,15 +42,19 @@ const Shop = () => {
       );
 };
 
-function Product(props){
-      return(
+function Product(props) {
+      console.log(props)
+      return (
             <div className='product'>
-                  <img src={props.product.img} alt="logo"/>
-                  <h3 style={{color:"red"}}>${props.product.wholePrice}.{props.product.priceFraction}</h3>
-                  <h5>{props.product.name}</h5>
+                  
+                  <img src={props.product.img} alt="logo" />
+                  <h2 style={{ color: "#cc1c39" }}>${props.product.price}</h2>
+                  <h6>By {props.product.seller}</h6>
+                  <a href="#">{props.product.name}</a>
+                  <h4>Only {props.product.stock} pieces left</h4>
                   <button>Add To Cart</button>
             </div>
-            
+
       )
 }
 
