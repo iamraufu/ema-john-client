6be38a44b3d56fcc268cd6a3289e9530560614ed
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-10 04:38:39 +0600

    remove button added to review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
       const [cart, setCart] = useState([]);
-
+      const removeProduct = (productKey) => {
+            const newCart = cart.filter(pd => pd.key !== productKey)
+            setCart(newCart);
+            removeFromDatabaseCart(productKey);
+      }
       useEffect(() => {
             // Cart data 
             const savedCart = getDatabaseCart();
@@ -22,7 +26,7 @@ const Review = () => {
                   <h3>Cart Items: {cart.length}</h3>
                   <div className="product-container">
                         {
-                              cart.map(pd => <ReviewItem product={pd} key={pd.key}></ReviewItem>)
+                              cart.map(pd => <ReviewItem product={pd} key={pd.key} removeProduct={removeProduct}></ReviewItem>)
                         }
                   </div>
 
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const ReviewItem = (props) => {
-      const {name,quantity,img,price,seller} = props.product;
+      const {name,quantity,img,price,seller,key} = props.product;
+
       return (
             <div className='product'>
                   <img src={img} alt="logo" />
@@ -9,7 +10,7 @@ const ReviewItem = (props) => {
                   <h6>By <small>{seller}</small></h6>
                   <h4>{name}</h4>
                   <h5>Quantity: {quantity}</h5>
-                  <button> Remove</button>
+                  <button onClick={() => props.removeProduct(key)}> Remove</button>
             </div>
         
       );
