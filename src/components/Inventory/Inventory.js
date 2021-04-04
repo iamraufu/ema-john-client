3f08a4909f639937def6582abf6d5f59369f2e00
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-04 14:52:12 +0600

    all data added to database

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import fakeData from '../../fakeData';
 const Inventory = () => {
+      const handleClick =()=>{
+            fetch('http://localhost:5000/addProduct',{
+                  method:"POST",
+                  headers:{"Content-Type":"application/json"},
+                  body:JSON.stringify(fakeData)
+            })
+            .then(res=>res.json())
+      };
       return (
             <div>
-                  <h1>Inventory Coming Soon</h1>
+                  <button onClick={handleClick}>Click to add All Data</button>
             </div>
       );
 };
