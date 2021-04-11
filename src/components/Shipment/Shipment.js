da10f791971225b6182a67f4ab8bbc8b81e7280d
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-11 13:47:54 +0600

    order placed and saved to db

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,63 +1,74 @@
-import './Shipment.css';
-import React, { useContext } from 'react';
-import { UserContext } from "../../App";
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import './Shipment.css';
+import { useContext } from 'react';
+import { UserContext } from '../../App';
 import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
 import ProcessPayment from '../ProcessPayment/ProcessPayment';
 
 const Shipment = () => {
-    const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmit = data => {
-        const savedCart = getDatabaseCart();
-        const orderDetails = { ...loggedInUser, products: savedCart, shipment: data, orderTime: new Date() };
+  const { register, handleSubmit, watch, errors } = useForm();
+  // eslint-disable-next-line
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const [shippingData, setShippingData] = useState(null);
 
-        fetch('https://emaajohn.herokuapp.com/addOrder', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(orderDetails)
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data) {
-                    processOrder();
-                    alert('Your Order Placed Successfully!');
-                }
-            })
+  const onSubmit = data => {
+    setShippingData(data);
+  };
 
+  const handlePaymentSuccess = paymentId => {
+    const savedCart = getDatabaseCart();
+    const orderDetails = { 
+      ...loggedInUser, 
+      products: savedCart, 
+      shipment: shippingData, 
+      paymentId,
+      orderTime: new Date() 
     };
 
-    // eslint-disable-next-line
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    console.log(watch("example"));
+    fetch('https://emaajohn.herokuapp.com/addOrder', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(orderDetails)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data) {
+          processOrder();
+          // alert('your order placed successfully');
+        }
+      })
+  }
 
-    return (
+  console.log(watch("example")); // watch input value by passing the name of it
 
-        <div className="row">
-            <div className="col-md-6">
-                <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-                    <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
-                    {errors.name && <span className="error">Name is required</span>}
+  return (
+    <div className="row">
+      <div style={{display: shippingData ? 'none': 'block'}} className="col-md-6">
+        <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+          <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
+          {errors.name && <span className="error">Name is required</span>}
 
-                    <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
-                    {errors.email && <span className="error">Email is required</span>}
+          <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
+          {errors.email && <span className="error">Email is required</span>}
 
-                    <input name="address" ref={register({ required: true })} placeholder="Your Address" />
-                    {errors.address && <span className="error">Address is required</span>}
+          <input name="address" ref={register({ required: true })} placeholder="Your Address" />
+          {errors.address && <span className="error">Address is required</span>}
 
-                    <input name="phone" ref={register({ required: true })} placeholder="Your Phone Number" />
-                    {errors.phone && <span className="error">Phone Number is required</span>}
+          <input name="phone" ref={register({ required: true })} placeholder="Your Phone Number" />
+          {errors.phone && <span className="error">Phone Number is required</span>}
 
-                    <input type="submit" />
-                </form>
-            </div>
-            <div className="col-md-6">
-                <h1>Pay Now!</h1>
-                <ProcessPayment />
-            </div>
-        </div>
-    );
+          <input type="submit" />
+        </form>
+      </div>
+      <div style={{display: shippingData ? 'block': 'none'}} className="col-md-6">
+        <h2>Please Pay for me</h2>
+        <ProcessPayment handlePayment={handlePaymentSuccess}></ProcessPayment>
+      </div>
+    </div>
+  );
 };
 
 export default Shipment;
