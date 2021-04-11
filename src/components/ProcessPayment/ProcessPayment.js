6e4b997f708f006e2a9be05c25a1bae2332867af
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-11 13:49:16 +0600

    stripe payment added and order placed to database

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-// import SimpleCardForPayment from './SimpleCardForPayment';
-import SpiltCardForm from './SpiltCardForm';
+import SimpleCardForm from './SimpleCardForm';
+// import SpiltCardForm from './SpiltCardForm';
 
-const ProcessPayment = () => {
+const ProcessPayment = ({handlePayment}) => {
       const stripePromise = loadStripe('pk_test_51IevUCA0Ihq6EpA0dqfdO8X4gU8CdMvb99F6l9RFCA3hibadCuDAMqlzj9mgvqmykisBnLM1PmXVGB9m4Bk1elVc00mNzURU0t');
 
       
       return (
 
             <Elements stripe={stripePromise}>
-                  {/* <SimpleCardForPayment/> */}
-                  <SpiltCardForm/>
+                  <SimpleCardForm handlePayment={handlePayment}></SimpleCardForm>
+                  {/* <SpiltCardForm/> */}
             </Elements>
 
       );
